7e88397aa7ee4c1935255756fc3ce6f5f8c5a089
Logan Schmillen <schmillen20@gmail.com>
2024-12-16 23:43:56 -0600

    initial commit

---
+++ src/main/java/com/example/watermap/WatermapApplication.java
@@ -0,0 +1,13 @@
+package com.example.watermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/watermap/WatermapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watermap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
