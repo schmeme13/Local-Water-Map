cefcf872ba43c209dd13d2738854e37f62af198a
Logan Schmillen <schmillen20@gmail.com>
2025-01-20 13:36:24 -0600

    cleaning

--- src/main/java/com/example/watermap/controller/WaterDataController.java
+++ src/main/java/com/example/watermap/controller/WaterDataController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
